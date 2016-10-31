293bef2b9aa0f378c538a2cce8e062f281e5dbce
Hebert Porto <hebertporto@gmail.com>
2016-10-31 18:45:41 -0200

    refatoracao de exemplo no controller

--- www/js/controllers/infoGatherer.controller.js
+++ www/js/controllers/infoGatherer.controller.js
@@ -21,3 +21,35 @@
   }
 
 })();
+
+// Hebert
+
+(function() {
+  'use strict';
+  angular.module('mtgx.controllers')
+         .controller('InfoGathererCtrl', InfoGathererCtrl);
+
+         InfoGathererCtrl.$inject = ['InfoGathererService'];
+
+          function InfoGathererCtrl (InfoGathererService){
+
+              var vm = this;
+              // variaveis que vão para view declaradas no topo -- Agrupamento de caracteristicas em Objetos. 
+              vm.findCard = findCard();
+              vm.ObjCard  = {};
+
+              // function para isolar todas sa funcionalidades do controller
+              function findCard () {
+                  InfoGathererService.getCard(vm.multiverseID)
+                  .then(function(card) {
+                        vm.ObjCard.rulings  = card.rulings;
+                        vm.ObjCard.name     = card.name;
+                        vm.ObjCard.imgURL   = card.imageUrl;
+                    console.log(card);
+                  }, function(error) {
+                    console.log("promessa getRulings voltou com falha");
+                  });
+              }
+          }
+
+})();
